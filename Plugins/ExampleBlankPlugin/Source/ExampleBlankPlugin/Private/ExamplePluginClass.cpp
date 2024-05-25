cef0e3e9965182d8debaa34ff88fe97dd6c86f10
Avatarek78 <avatarek78@gmail.com>
2024-05-25 22:44:41 +0200

    Shortened description of earlier error
    - this error is gone, so I shortened the full error log in comment

--- Plugins/ExampleBlankPlugin/Source/ExampleBlankPlugin/Private/ExamplePluginClass.cpp
+++ Plugins/ExampleBlankPlugin/Source/ExampleBlankPlugin/Private/ExamplePluginClass.cpp
@@ -17,31 +17,9 @@ UExamplePluginClass* UExamplePluginClass::getInstance() {
     if (instance == nullptr) {
         instance = NewObject<UExamplePluginClass>();
         /**
-        * "Prevent garbage collection" This sometimes leads to this ERROR in UE!:
-        * LogOutputDevice: Error: === Handled ensure: ===
-        * LogOutputDevice: Error: Ensure condition failed: bIsValidObjectReference  [File:D:\build\++UE5\Sync\Engine\Source\Runtime\Engine\Private\BlueprintGeneratedClass.cpp] [Line: 2370] 
-        * LogOutputDevice: Error: Invalid object referenced by the PersistentFrame: 0x000009773a4ba8c0 (Blueprint object: Blueprint /Game/FirstPerson/Blueprints/BP_FirstPersonPlayerController.BP_FirstPersonPlayerController, ReferencingProperty: ObjectProperty /Game/FirstPerson/Blueprints/BP_FirstPersonPlayerController.BP_FirstPersonPlayerController_C:ExecuteUbergraph_BP_FirstPersonPlayerController:CallFunc_getExamplePluginClass_ReturnValue, Instance: BP_FirstPersonPlayerController_C /Game/FirstPerson/Maps/UEDPIE_0_FirstPersonMap.FirstPersonMap:PersistentLevel.BP_FirstPersonPlayerController_C_0, Address:
-        *  0x000009773e217bf8) - If you have a reliable repro for this, please contact the development team with it.
-        * LogOutputDevice: Error: Stack: 
-        * LogOutputDevice: Error: [Callstack] 0x00007fff8bd7b572 UnrealEditor-Engine.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff898dfcbf UnrealEditor-Engine.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff95e0e574 UnrealEditor-CoreUObject.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff95e13b2c UnrealEditor-CoreUObject.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff898a063d UnrealEditor-Engine.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff95b48901 UnrealEditor-CoreUObject.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff95aa2963 UnrealEditor-CoreUObject.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff95b04a88 UnrealEditor-CoreUObject.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff95a971ee UnrealEditor-CoreUObject.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff95a39944 UnrealEditor-CoreUObject.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff93472485 UnrealEditor-Core.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff934721bf UnrealEditor-Core.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff9349a115 UnrealEditor-Core.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff93462390 UnrealEditor-Core.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff93666b13 UnrealEditor-Core.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff93acb7ad UnrealEditor-Core.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007fff93abf127 UnrealEditor-Core.dll!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007ff8daf87344 KERNEL32.DLL!UnknownFunction []
-        * LogOutputDevice: Error: [Callstack] 0x00007ff8db1226b1 ntdll.dll!UnknownFunction []
+        * "Prevent garbage collection" This earlier leads to this ERROR in UE!: 
+        * Error: Invalid object referenced by the PersistentFrame: 0x000009773a4ba8c0 (Blueprint object: Blueprint /Game/FirstPerson/...
+        * Now seems OK
         */
         instance->AddToRoot(); // Prevent garbage collection
         Log("New instance of UExamplePluginClass created.", ELogLevel::INFO);
